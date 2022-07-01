95bfd9a6e3481812234251d21b68f201fbd8719c
Raj Colaco <55150453+roco007@users.noreply.github.com>
2022-07-01 14:07:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-String.prototype.allReplace = function(obj) {
-    var retStr = this;
-    for (var x in obj) {
-        retStr = retStr.replace(new RegExp(x, 'g'), obj[x]);
-    }
-    return retStr;
-};
-
-async function process(text){
-    ut =await text.allReplace({'$':'','^':'','#':'','!':''});
-    console.log(ut);
-    return ut;
-}
-
-
 async function button() {
 
 let [fileHandle] = await window.showOpenFilePicker();
 let fileData = await fileHandle.getFile();
 let text = await fileData.text();
-let r =await process(text)
+let r = text.replaceAll("#","").replaceAll("^","").replaceAll("!","").replaceAll("$","");
 document.getElementById("output").innerHTML = r;
 document.getElementById("w_count").innerHTML = "Word Count :"+ r.split(" ").length;
 }
